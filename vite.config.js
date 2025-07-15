eafb3e7157a09c30ad2dc19148c9621b25bb4dcf
Elias Awad <eliasmsilaptop@gmail.com>
2025-07-15 19:53:39 +0300

    Fix: remove chmod script, clean dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    outDir: 'dist', // folder where build files are output
+    sourcemap: false, // optionally true if you want source maps for debugging
+  }
+})
